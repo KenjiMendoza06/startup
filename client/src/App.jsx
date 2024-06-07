bde64c1595325e73dd9bb47293008e8c4f7569a9
KenjiMendoza06 <mendozaken010601@gmail.com>
2024-06-07 09:12:03 +0800

    startup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route>
+        </Route>
+      </Routes>
+    </div>
+  )
+}
